fixed normalisation

acoustic_utils.py
@@ -78,7 +78,59 @@ def t60_impulse(raw_signal,fs, bands, rt='t30'):  # pylint: disable=too-many-loc
         t60[band] = factor * (db_regress_end - db_regress_init)
     return t60
 
+
+#---------------------------------------------------------------------------------
+def t60_impulse_avg(raw_signal,fs, rt='t30'):  # pylint: disable=too-many-locals
+    """
+    Average reverberation time from a WAV impulse response.
+    :param file_name: name of the WAV file containing the impulse response.
+    :param rt: Reverberation time estimator. It accepts `'t30'`, `'t20'`, `'t10'` and `'edt'`.
+    :returns: Reverberation time :math:`T_{60}`
+    """
+
+    rt = rt.lower()
+    if rt == 't30':
+        init = -5.0
+        end = -35.0
+        factor = 2.0
+    elif rt == 't20':
+        init = -5.0
+        end = -25.0
+        factor = 3.0
+    elif rt == 't10':
+        init = -5.0
+        end = -15.0
+        factor = 6.0
+    elif rt == 'edt':
+        init = 0.0
+        end = -10.0
+        factor = 6.0
+
+
+    # Filtering signal
+    abs_signal = np.abs(raw_signal) / np.max(np.abs(raw_signal))
+
+    # Schroeder integration
+    sch = np.cumsum(abs_signal[::-1]**2)[::-1]
+    sch_db = 10.0 * np.log10(sch / np.max(sch))
+
+    # Linear regression
+    sch_init = sch_db[np.abs(sch_db - init).argmin()]
+    sch_end = sch_db[np.abs(sch_db - end).argmin()]
+    init_sample = np.where(sch_db == sch_init)[0][0]
+    end_sample = np.where(sch_db == sch_end)[0][0]
+    x = np.arange(init_sample, end_sample + 1) / fs
+    y = sch_db[init_sample:end_sample + 1]
+    slope, intercept = stats.linregress(x, y)[0:2]
+
+    # Reverberation time (T30, T20, T10 or EDT)
+    db_regress_init = (init - intercept) / slope
+    db_regress_end = (end - intercept) / slope
+    t60 = factor * (db_regress_end - db_regress_init)
+    return t60
 #------------------------------------------------------------------------------------------
+
+
 """def rir2t60(rir_dir, output_folder):
     t60s = []
     rir_list = sorted(glob.glob(rir_dir + '*.wav'))
@@ -105,6 +157,26 @@ def t60_impulse(raw_signal,fs, bands, rt='t30'):  # pylint: disable=too-many-loc
         f.close()
     return t60s
 """
+#------------------------------------------------------------------------------------------
+
+
+def clarity_avg(time, signal, fs):
+    """
+    Clarity :math:`C_i` determined from an impulse response.
+
+    :param time: Time in miliseconds (e.g.: 50, 80).
+    :param signal: Impulse response.
+    :type signal: :class:`np.ndarray`
+    :param fs: Sample frequency.
+    :param bands: Bands of calculation (optional). Only support standard octave and third-octave bands.
+    :type bands: :class:`np.ndarray`
+
+    """
+    h2 = signal**2.0
+    t = int((time / 1000.0) * fs + 1)
+    c = 10.0 * np.log10((np.sum(h2[:t]) / np.sum(h2[t:])))
+    return c
+
 #------------------------------------------------------------------------------------------
 def rir2clarity(rir_dir, output_folder,time):
     #Process all RIRs and save outputs as pkl
@@ -239,21 +311,21 @@ def drr(signal, fs, bands=None, debug = False):
     return drr
 
 #------------------------------------------------------------------------------------------
-def rir2drr(rir_dir, output_folder, bands=None):
+def rir2drr(rir_dir, output_folder = None, bands=None):
     
     rir_list = sorted(glob.glob(rir_dir + '*.wav'))
-    a=1
-    try:
-        os.mkdir(output_folder + "drr")
-    except:
-        print("drr folder already exist")
+    
+    if output_folder is not None:
+        try:
+            os.mkdir(output_folder + "drr")
+        except:
+            print("drr folder already exist")
         
     if bands is not None:
         drrs = np.empty((len(rir_list),len(bands)))
     else:
         drrs = np.empty(len(rir_list))
         
-    print(f'drr size = {drrs.shape}')
     #Process all RIRs and save outputs as pkl
     for i, rir_file in enumerate(tqdm(rir_list)):
         #print(f'RIR --> {rir_file}')
@@ -271,9 +343,11 @@ def rir2drr(rir_dir, output_folder, bands=None):
         #print(drrs)
         
         filename = os.path.splitext(os.path.basename(rir_file))[0]
-        with open(output_folder + "drr/" + filename + ".pkl", "wb") as f:
-            pickle.dump(drrs[i], f)
-        f.close()
+        
+        if output_folder is not None :
+            with open(output_folder + "drr/" + filename + ".pkl", "wb") as f:
+                pickle.dump(drrs[i], f)
+            f.close()
         
     #print(f'drr after log = {drrs}\n')
     return drrs

convolute.py
@@ -1,24 +1,76 @@
 #Generates reverberant music and speech
-from glob import glob
-from tqdm import tqdm
+import glob
+import tqdm
 from scipy.io import wavfile
 from normalize import *
 import os
+from argparse import ArgumentParser
+from scipy import signal
 
-music_dir =
-rir_dir = 
-out_dir = 
+def parse_args():
+    parser = ArgumentParser(description='MakeConvDataset')
+    
+    parser.add_argument(
+        '-music_dir', '--music_dir',
+        type=str, default=None, required=True,
+        help='Music directory.'
+    )
+    
+    parser.add_argument(
+        '-rir_dir', '--rir_dir',
+        type=str, default=None, required = True,
+        help='rir directory'
+    )
+    
+    parser.add_argument(
+        '-out_dir', '--out_dir',
+        type=str, default=None, required = True,
+        help='output directory'
+    )
+    
+    parser.add_argument(
+        '-trim', '--trim',
+        type=int, default=None, 
+        help='output directory'
+    )
+    
+    return parser.parse_args()
+    
+if __name__ == '__main__':
+    # Parse command-line arguments
+    args = parse_args()
+    
+    music_dir = args.music_dir
+    rir_dir = args.rir_dir
+    out_dir = args.out_dir
 
 
-#normalize the samples first 
-normed_music_dir = normalize_audio_dir(music_dir,trim=4)
-normed_rir_dir = normalize_audio_dir(rir_dir)
+    #resample audio first 
+    print('---norm music+rir directories---')
+    normed_music_dir = resample_audio_dir(music_dir,trim=args.trim)
+    normed_rir_dir = resample_audio_dir(rir_dir)
 
+    if not os.path.exists(out_dir):
+        os.makedirs(out_dir)
 
-if not os.path.exists(out_dir):
-    os.makedirs(out_dir)
+    print('---Convolving...---')
+    for music_file in tqdm.tqdm(glob.glob(normed_music_dir + '*')):
 
-for music_file in tqdm(glob(normed_music_dir)):
-    wavfile.read(music_file)
-    for rir_file in tqdm(glob(normed_rir_dir)):
+        m_sr, music_sig = wavfile.read(music_file)
+        music_sig = music_sig / np.max(np.abs(music_sig))
         
+        music_name = os.path.splitext(os.path.basename(music_file))[0]
+        
+        for rir_file in glob.glob(normed_rir_dir + '*'):
+            rir_sr, rir_sig = wavfile.read(rir_file)
+            
+            rir_sig = rir_sig / np.max(np.abs(rir_sig))
+                        
+            music_rev = signal.fftconvolve(music_sig, rir_sig, mode="full")
+            music_rev = music_rev[:len(music_sig)]
+            
+            music_rev = music_rev / np.max(np.abs(music_rev))
+
+            wavfile.write(out_dir + music_name + '-' + os.path.basename(rir_file), m_sr, music_rev)
+            
+    print('---Normalizing outputs---')

mfcc.py
@@ -19,6 +19,21 @@ def feature_normalize(dataset):
     return (dataset - np.mean(dataset, axis=0)) / np.abs(dataset).max(axis=0)
 
 #------------------------------------------------------------------------------------------
+def compute_norm_mfcc(signal,fs,n_bands,hop_len,n_fft,window_size):
+    signal = feature_normalize(signal)
+    mfcc = librosa.feature.mfcc(y=signal, sr=fs, n_mfcc= n_bands,hop_length=hop_len, n_fft=n_fft)
+    mfcc = librosa.util.fix_length(mfcc, window_size, axis=1, mode='wrap') #Reshape to windom size
+    mfcc = librosa.util.normalize(mfcc, axis=1)
+    return mfcc
+
+#------------------------------------------------------------------------------------------
+def compute_norm_mfcc_2(signal,fs):
+    signal = feature_normalize(signal)
+    mfcc = librosa.feature.mfcc(y=signal, sr=fs)
+    #mfcc = librosa.util.fix_length(mfcc, window_size, axis=1, mode='wrap') #Reshape to windom size
+    mfcc = librosa.util.normalize(mfcc, axis=1)
+    return mfcc
+#------------------------------------------------------------------------------------------
 def compute_music_mfccs(music_dir,output_folder,window_size = 645, mfcc_bands = 40, mfcc_degree = 0,
                        chunk_duration_s = 30):
     
@@ -42,19 +57,13 @@ def compute_music_mfccs(music_dir,output_folder,window_size = 645, mfcc_bands = 
         filename = os.path.splitext(os.path.basename(music_name))[0]
 
         m_sr, music =  wavfile.read(music_name)
-        music = feature_normalize(music)
-        """try: 
-            
-            #music = music[:chunck_duration_s*m_sr]
-            
-            
-        except Exception as e:
-            print("Error encountered while parsing file: ", music_name)
-            return None 
-        """
-        mfcc = librosa.feature.mfcc(y=music, sr=m_sr, n_mfcc= mfcc_bands,hop_length=512, n_fft=1024)
-        mfcc = librosa.util.fix_length(mfcc, window_size, axis=1, mode='wrap') #Reshape to windom size
-        mfcc = librosa.util.normalize(mfcc, axis=1)
+        
+        mfcc = compute_norm_mfcc(signal = music,
+                                 fs = m_sr,
+                                 n_bands = mfcc_bands,
+                                 hop_len=512,
+                                 n_fft = 1024,
+                                 window_size = window_size)
             
         if mfcc_degree == 1:
             mfcc_delta = librosa.feature.delta(mfcc)