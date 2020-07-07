a200559c9bb941721ff4388e7f2eec6a0e91d2f8
polocallens <pcallens@logitech.com>
2020-07-07 17:10:51 +0200

    adapted training function

--- train_from_rev.py
+++ train_from_rev.py
@@ -47,9 +47,15 @@ def parse_args():
     parser = ArgumentParser(description='Training')
     
     parser.add_argument(
-        '-dataDir', '--dataDir',
+        '-xDir', '--xDir',
         type=str, default=None, required=True,
-        help='Data directory.'
+        help='Mfvv data directory.'
+    )
+    
+    parser.add_argument(
+        '-yDir', '--yDir',
+        type=str, default=None, required=True,
+        help='true acoustic values (from rir) directory.'
     )
     
     parser.add_argument(
@@ -72,7 +78,7 @@ def parse_args():
     
     parser.add_argument(
         '-window_size', '--window_size',
-        type=int, default=645,
+        type=int, default=251,
         help='Number of timesteps.'
     )
     
@@ -90,13 +96,13 @@ def parse_args():
     
     parser.add_argument(
         '-n_channels', '--n_channels',
-        type=int, default=2,
+        type=int, default=1,
         help='Numbers of mfccs stacked for input'
     )
     
     parser.add_argument(
         '-output_size', '--output_size',
-        type=int, default=13,
+        type=int, default=12,
         help='output shapes [number of freq bands]'
     )
     
@@ -219,9 +225,9 @@ shuffle
 """    
 
 
-class DataGenerator_loader(Sequence):
+class DataGenerator(Sequence):
     'Generates data for Keras'
-    def __init__(self, dataset_folder, list_IDs,
+    def __init__(self, y_dir, ds_paths,
                  window_size, mfcc_bands,
                  mfcc_degree, y_param,
                  n_channels,
@@ -229,8 +235,8 @@ class DataGenerator_loader(Sequence):
                  batch_size, 
                  shuffle=True):
         'Initialization'
-        self.list_IDs = list_IDs
-        self.dataset_folder = dataset_folder
+        self.ds_paths = ds_paths
+        self.y_dir = y_dir
         self.window_size = window_size
         self.mfcc_bands = mfcc_bands
         self.y_param = y_param
@@ -238,7 +244,6 @@ class DataGenerator_loader(Sequence):
         self.batch_size = batch_size
         self.shuffle = shuffle
         self.output_size = output_size
-        self.bands = acoustics.bands.third(500,8000)
         self.params = ['t60','drr','c50','c80']
         #print(f'output size is {self.output_size}')
         #all combinaisons of music and rirs # format = [music_name, rir_name]
@@ -247,29 +252,30 @@ class DataGenerator_loader(Sequence):
 
     def __len__(self):
         'Denotes the number of batches per epoch'
-        return int(np.floor(len(self.list_IDs) / self.batch_size))
+        return int(np.floor(len(self.ds_paths) / self.batch_size))
 
     def __getitem__(self, index):
         'Generate one batch of data'
         # Generate indexes of the batch
         indexes = self.indexes[index*self.batch_size:(index+1)*self.batch_size]
         # Find list of IDs
-        list_IDs_temp = [self.list_IDs[k] for k in indexes]
+        list_paths_temp = [self.ds_paths[k] for k in indexes]
 
         # Generate data
-        X, y = self.__data_generation(list_IDs_temp)
+        X, y = self.__data_generation(list_paths_temp)
         
         return X, y
 
     def on_epoch_end(self):
         'Updates indexes after each epoch'
-        self.indexes = np.arange(len(self.list_IDs))
+        self.indexes = np.arange(len(self.ds_paths))
         if self.shuffle == True:
             np.random.shuffle(self.indexes)
 
-    def __data_generation(self, list_IDs_temp):
+    def __data_generation(self, list_paths_temp):
         'Generates data containing batch_size samples' # X : (n_samples, *dim, n_channels)
         ###### X and y Initialization
+
         X = np.empty((self.batch_size, self.mfcc_bands, self.window_size, self.n_channels))
        
         if self.y_param == 'all':
@@ -282,27 +288,31 @@ class DataGenerator_loader(Sequence):
         ###### Generate data
         
         #Load mfccs
-        for i, index in enumerate(list_IDs_temp):
+        for i, path in enumerate(list_paths_temp):
             
-            with open(self.dataset_folder + 'X/' + str(index) + '.pkl', "rb") as f:
-                X[i] = pickle.load(f)
+            with open(path, "rb") as f:
+                X[i] = pickle.load(f).reshape(1, self.mfcc_bands, self.window_size, self.n_channels)
             f.close()
+            
             if(np.isnan(np.sum(X[i]))):
                 print(f'\nWARNING : Nan value found in {i}')            
 
             #Load output
+            
+            room_name = os.path.split(os.path.dirname(path))[1]
+            
             if self.y_param == 'all':
                 for j,param in enumerate(self.params):
-                    with open(self.dataset_folder + 'y/' + str(param) + '/' + str(index) + '.pkl', "rb") as f:
+                    with open(self.y_dir + param + '/' + room_name + '.pkl', "rb") as f:
                         y[i,j] = pickle.load(f)
                     f.close()
             else:
-                with open(self.dataset_folder + 'y/' + self.y_param + '/' + str(index) + '.pkl', "rb") as f:
+                with open(self.y_dir + self.y_param + '/' + room_name + '.pkl', "rb") as f:
                     y[i] = pickle.load(f)
                 f.close()
                 
                 if(np.isnan(np.sum(y[i]))):
-                    print(f'\nfuckin hell y --> {index}')
+                    print(f'\nWARNING : Nan value found in {index}')
                     #if i==1: print(f'\ny value = {y[i]}\n')
         return X, y
     
@@ -325,7 +335,7 @@ if __name__ == '__main__':
     tfback._get_available_gpus = _get_available_gpus
 
     os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"
-    os.environ["CUDA_VISIBLE_DEVICES"]= "1"
+    os.environ["CUDA_VISIBLE_DEVICES"]= str(args.gpu)
 
     
     print("\n\n-----Parameters-----\n")
@@ -334,21 +344,25 @@ if __name__ == '__main__':
     print('\n')
     
     
-    nb_samples = len(glob.glob(args.dataDir+'X/*'))
+    """nb_samples = len(glob.glob(args.dataDir+'X/*'))
     print(f'nb samples = {nb_samples}')
-    indexes = np.arange(nb_samples)
-
+    indexes = np.arange(nb_samples)"""
+    
+    print("parsing x file paths...")
+    paths_list = glob.glob(args.xDir + '*/*.pkl')
+    print('done!')
+    
     #Split train/test
-    train_indexes, test_indexes = train_test_split(indexes,test_size=0.2, random_state=42)
+    train_ds, test_ds = train_test_split(paths_list,test_size=0.2, random_state=42)
 
     # Generators
-    training_generator = DataGenerator_loader(args.dataDir, train_indexes, **params)
-    validation_generator = DataGenerator_loader(args.dataDir, test_indexes, **params)
+    training_generator = DataGenerator(args.yDir, train_ds, **params)
+    validation_generator = DataGenerator(args.yDir, test_ds, **params)
 
     # Define model
-    #model = eval(args.net)((None,params['mfcc_bands'], params['window_size'],params['n_channels']), args.output_size)
+    model = eval(args.net)((None,params['mfcc_bands'], params['window_size'],params['n_channels']), args.output_size)
 
-    model = CRNN2D((None,40, 645 ,2), 13)
+    #model = CRNN2D((None,40, 645 ,2), 13)
     
     #load previous weights
     if(args.load_weights):
@@ -363,6 +377,8 @@ if __name__ == '__main__':
     checkpointer = ModelCheckpoint(filepath='weights.best.' + str(args.name) + '.hdf5', 
                                    verbose=1, save_best_only=True)
 
+    if not os.path.exists('logs'):
+        os.makedirs('logs')
     logdir = "logs/" + str(args.name)# + datetime.now().strftime("%Y%m%d-%H%M%S")
     tensorboard = TensorBoard(log_dir=logdir,profile_batch=0,update_freq='batch')
     
