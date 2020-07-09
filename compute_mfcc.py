9db1747131a864e35cf526d3bb4195b9f184578f
polocallens <pcallens@logitech.com>
2020-07-09 15:49:56 +0200

    implemented separate output directory

--- compute_mfcc.py
+++ compute_mfcc.py
@@ -12,22 +12,34 @@ def parse_args():
     parser = ArgumentParser(description='MakeConvDataset')
     
     parser.add_argument(
-        '-rev_dir', '--rev_dir',
+        '-revDir', '--revDir',
         type=str, default=None, required=True,
-        help='Music directory.'
+        help='Reveberant signals directory.'
     )
+    
+    parser.add_argument(
+        '-outDir', '--outDir',
+        type=str, default=None, required=True,
+        help='mfcc output directory'
+    )
+        
     return parser.parse_args()
 
     
 if __name__ == '__main__':
     # Parse command-line arguments
     args = parse_args()
-    
-    music_dir = args.rev_dir
-    
-    for room in tqdm(glob.glob(music_dir + '*')):
+        
+    for room in tqdm(glob.glob(args.revDir + '*')):
+        room_name = os.path.split(room)[1]
+        if not os.exists(args.outDir + room_name):
+            os.makedirs(args.outDir + room_name)
+            
         for audio_sample_path in tqdm(glob.glob(room + '/*.wav')):
             sr_audio, audio = wavfile.read(audio_sample_path)
             mfcc = compute_norm_mfcc_3(audio,sr_audio)
-            with open(os.path.splitext(audio_sample_path)[0] + '.pkl','wb') as pkl_file:
+            
+            audio_name = os.path.split(os.path.splitext(audio_sample_path)[0])[1]
+            
+            with open(args.outDir + room_name + '/' + audio_name + '.pkl','wb') as pkl_file:
                 pickle.dump(mfcc,pkl_file)
