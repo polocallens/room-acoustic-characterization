1d530be2960807ea57b690b55fd6318dc6d2cfdd
polocallens <pcallens@logitech.com>
2020-08-03 10:18:13 +0200

    added dense layers for better regression

--- utils/models.py
+++ utils/models.py
@@ -3,13 +3,7 @@ from keras.layers import Permute
 from keras.layers import Conv1D, MaxPooling1D
 from keras.layers.recurrent import GRU, LSTM
 from tensorflow.keras.utils import Sequence
-from tensorflow import keras
-
-
-#from keras.models import Sequential, Model
-#from keras.layers import Dense, Dropout, Activation, Flatten, CuDNNLSTM, BatchNormalization
-#from keras.layers import Convolution2D, Conv2D, MaxPooling2D, GlobalAveragePooling2D
-#from keras.layers import Input, Reshape, TimeDistributed
+#from tensorflow import keras
 
 from tensorflow.keras.callbacks import TensorBoard
 from tensorflow.keras.layers import *
@@ -74,14 +68,10 @@ def CRNN2D(X_shape, nb_classes):
     #model.add(Activation("relu"))
     return model
 
-
 #---------------------------------------------------------------------------------
 
-def CRNN2D_parallel(X_shape, nb_classes):
-    '''
-    Model used for evaluation in paper. Inspired by K. Choi model in:
-    https://github.com/keunwoochoi/music-auto_tagging-keras/blob/master/music_tagger_crnn.py
-    '''
+
+def CRNN2D_2(X_shape, nb_classes):
 
     nb_layers = 4  # number of convolutional layers
     nb_filters = [64, 128, 128, 128]  # filter sizes
@@ -116,8 +106,7 @@ def CRNN2D_parallel(X_shape, nb_classes):
         model.add(Conv2D(nb_filters[layer + 1], kernel_size=kernel_size,
                          padding='same'))
         model.add(Activation(activation))
-        model.add(BatchNormalization(
-            axis=channel_axis))  # Improves overfitting/underfitting
+        model.add(BatchNormalization(axis=channel_axis))  # Improves overfitting/underfitting
         model.add(MaxPooling2D(pool_size=pool_size[layer + 1],
                                strides=pool_size[layer + 1],
                               data_format="channels_first"))  # Max pooling
@@ -135,10 +124,11 @@ def CRNN2D_parallel(X_shape, nb_classes):
     model.add(Dropout(0.3))
 
     # Output layer
+    model.add(Dense(128))
+    model.add(Dense(64))
     model.add(Dense(nb_classes))
-    model.add(Activation("relu"))
+    #model.add(Activation("relu"))
     return model
-
 #---------------------------------------------------------------------------------
 
 def CRNN2D_classifier(X_shape, nb_classes):
@@ -202,73 +192,6 @@ def CRNN2D_classifier(X_shape, nb_classes):
     return model
 
 
-"""##DOES NOT CONVERGE
-def CNN(X_shape, nb_classes):
-
-    nb_layers = 3  # number of convolutional layers
-    nb_filters = [64, 128, 256, 8]  # filter sizes
-    kernel_size = (3, 3)  # convolution kernel size
-    activation = 'elu'  # activation function to use after each layer
-    pool_size = [(3, 3), (3, 3), (3, 3), (3, 3),
-                 (3, 3)]  # size of pooling area
-
-    # shape of input data (frequency, time, channels)
-    input_shape = (X_shape[1], X_shape[2], X_shape[3])
-    frequency_axis = 1
-    time_axis = 2
-    channel_axis = 3
-
-    # Create sequential model and normalize along frequency axis
-    model = Sequential()
-    
-    model.add(BatchNormalization(axis=frequency_axis, input_shape=input_shape))
-   
-    # First convolution layer specifies shape
-    model.add(Conv2D(nb_filters[0], kernel_size=kernel_size, padding='same',
-                     data_format="channels_last",
-                     input_shape=input_shape))
-    model.add(Activation(activation))
-    model.add(BatchNormalization(axis=channel_axis))
-    model.add(MaxPooling2D(pool_size=pool_size[0], strides=pool_size[0],))
-    model.add(Dropout(0.3))
-
-    # Add more convolutional layers
-    for layer in range(nb_layers - 1):
-        # Convolutional layer
-        model.add(Conv2D(nb_filters[layer + 1], kernel_size=kernel_size,
-                         padding='same'))
-        model.add(Activation(activation))
-        model.add(BatchNormalization(
-            axis=channel_axis))  # Improves overfitting/underfitting
-        model.add(MaxPooling2D(pool_size=pool_size[layer + 1],
-                               strides=pool_size[layer + 1],
-                              data_format="channels_first"))  # Max pooling
-        model.add(Dropout(0.3))
-
-        # Reshaping input for recurrent layer
-    # (frequency, time, channels) --> (time, frequency, channel)
-    #model.add(Permute((time_axis, frequency_axis, channel_axis)))
-    #resize_shape = model.output_shape[2] * model.output_shape[3]
-    #model.add(Reshape((model.output_shape[1])))
-    
-    # recurrent layer
-    model.add(GRU(32, return_sequences=True))
-    model.add(GRU(32, return_sequences=False))
-    model.add(Dropout(0.3))
-
-    model.add(Flatten())
-    
-    # Output layer
-    model.add(Dense(256))
-    model.add(Activation("relu"))
-    
-    model.add(Dense(64))
-    model.add(Activation("relu"))
-    
-    model.add(Dense(nb_classes))
-    model.add(Activation("relu"))
-    return model
-"""
 #---------------------------------------------------------------------------------
 def CNN1(X_shape, nb_classes):
 
