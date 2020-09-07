696a4c6a02ee63a0ca8b7c29b108337129731307
Milos Cernak <mcernak@logitech.com>
2020-09-07 14:59:44 +0200

    Add CNN2 model as Looney-and-Gaubitch ICASSP2020 joint estimation baseline, and CRNN2D_3 as a CRNN variant based on CNN2 (with added 2 GRU layears)

--- utils/models.py
+++ utils/models.py
@@ -9,6 +9,36 @@ from tensorflow.keras.callbacks import TensorBoard
 from tensorflow.keras.layers import *
 from tensorflow.keras.models import Sequential
 
+#---------------------------------------------------------------------------------
+def CRNN2D_5(X_shape, nb_classes):
+    frequency_axis = 1
+    time_axis = 2
+    channel_axis = 3
+
+    # Construct model
+    model = Sequential()
+    model.add(Conv2D(filters=256, kernel_size=(5, 5), input_shape=(X_shape[1], X_shape[2], X_shape[3]), activation='relu'))
+    model.add(MaxPooling2D(pool_size=2))
+    #model.add(Dropout(0.2))
+
+    model.add(Conv2D(filters=256, kernel_size=(5, 5), activation='relu'))
+    model.add(MaxPooling2D(pool_size=2))
+    model.add(GlobalAveragePooling2D())
+    model.add(Dropout(0.2))
+
+    #model.add(Permute((time_axis, frequency_axis, channel_axis)))
+    #resize_shape = model.output_shape[2] * model.output_shape[3]
+    model.add(Reshape((-1, 256)))
+
+    # recurrent layer
+    model.add(GRU(64, activation='relu', recurrent_activation='relu', return_sequences=True))
+    model.add(GRU(64, activation='relu', recurrent_activation='relu', return_sequences=False))
+
+    # Output layer
+    model.add(Dense(64, activation='relu'))
+    model.add(Dense(nb_classes))
+
+    return model
 
 def CRNN2D(X_shape, nb_classes):
 
@@ -69,8 +99,36 @@ def CRNN2D(X_shape, nb_classes):
     return model
 
 #---------------------------------------------------------------------------------
+def CRNN2D_3(X_shape, nb_classes):
+    frequency_axis = 1
+    time_axis = 2
+    channel_axis = 3
 
+    # Construct model
+    model = Sequential()
+    model.add(Conv2D(filters=256, kernel_size=(5, 5), input_shape=(X_shape[1], X_shape[2], X_shape[3]), activation='relu'))
+    model.add(MaxPooling2D(pool_size=2))
+    #model.add(Dropout(0.2))
 
+    model.add(Conv2D(filters=256, kernel_size=(5, 5), activation='relu'))
+    model.add(MaxPooling2D(pool_size=2))
+    model.add(GlobalAveragePooling2D())
+    model.add(Dropout(0.1))
+
+    #model.add(Permute((time_axis, frequency_axis, channel_axis)))
+    #resize_shape = model.output_shape[2] * model.output_shape[3]
+    model.add(Reshape((-1, 256)))
+
+    # recurrent layer
+    model.add(GRU(64, activation='relu', return_sequences=True))
+    model.add(GRU(64, activation='relu', return_sequences=False))
+
+    # Output layer
+    model.add(Dense(64, activation='relu'))
+    model.add(Dense(nb_classes))
+
+    return model
+#---------------------------------------------------------------------------------
 def CRNN2D_2(X_shape, nb_classes):
 
     nb_layers = 4  # number of convolutional layers
@@ -130,6 +188,67 @@ def CRNN2D_2(X_shape, nb_classes):
     #model.add(Activation("relu"))
     return model
 #---------------------------------------------------------------------------------
+# ---------------------------------------------------------------------------------
+def CRNN2D_4(X_shape, nb_classes):
+    nb_layers = 4  # number of convolutional layers
+    nb_filters = [64, 128, 128, 256]  # filter sizes
+    kernel_size = (5, 5)  # convolution kernel size
+    activation = 'elu'  # activation function to use after each layer
+    pool_size = [(2, 2), (4, 2), (4, 2), (4, 2),
+                 (4, 2)]  # size of pooling area
+
+    # shape of input data (frequency, time, channels)
+    input_shape = (X_shape[1], X_shape[2], X_shape[3])
+    frequency_axis = 1
+    time_axis = 2
+    channel_axis = 3
+
+    # Create sequential model and normalize along frequency axis
+    model = Sequential()
+
+    model.add(BatchNormalization(axis=frequency_axis, input_shape=input_shape))
+
+    # First convolution layer specifies shape
+    model.add(Conv2D(nb_filters[0], kernel_size=kernel_size, padding='same',
+                     data_format="channels_last",
+                     input_shape=input_shape))
+    model.add(Activation(activation))
+    model.add(BatchNormalization(axis=channel_axis))
+    model.add(MaxPooling2D(pool_size=pool_size[0], strides=pool_size[0], ))
+    model.add(Dropout(0.1))
+
+    # Add more convolutional layers
+    for layer in range(nb_layers - 1):
+        # Convolutional layer
+        model.add(Conv2D(nb_filters[layer + 1], kernel_size=kernel_size,
+                         padding='same'))
+        model.add(Activation(activation))
+        model.add(BatchNormalization(axis=channel_axis))  # Improves overfitting/underfitting
+        model.add(MaxPooling2D(pool_size=pool_size[layer + 1],
+                               strides=pool_size[layer + 1],
+                               data_format="channels_first"))  # Max pooling
+        model.add(Dropout(0.1))
+
+        # Reshaping input for recurrent layer
+    # (frequency, time, channels) --> (time, frequency, channel)
+    model.add(Permute((time_axis, frequency_axis, channel_axis)))
+    resize_shape = model.output_shape[2] * model.output_shape[3]
+    model.add(Reshape((model.output_shape[1], resize_shape)))
+
+    # recurrent layer
+    model.add(GRU(64, return_sequences=True))
+    model.add(GRU(32, return_sequences=False))
+    model.add(Dropout(0.3))
+
+    # Output layer
+    #model.add(Dense(128, activation=activation))
+    model.add(Dense(64, activation=activation))
+    model.add(Dense(nb_classes))
+    # model.add(Activation("relu"))
+    return model
+
+
+# ---------------------------------------------------------------------------------
 
 def CRNN2D_classifier(X_shape, nb_classes):
     '''
@@ -189,6 +308,35 @@ def CRNN2D_classifier(X_shape, nb_classes):
     # Output layer
     model.add(Dense(nb_classes))
     model.add(Activation("softmax"))
+    return model
+
+#---------------------------------------------------------------------------------
+def CNN2(X_shape, nb_classes):
+    # Looney, David, and Nikolay D. Gaubitch. "Joint Estimation Of Acoustic Parameters From Single-Microphone Speech Observations." ICASSP 2020-2020 IEEE International Conference on Acoustics, Speech and Signal Processing (ICASSP). IEEE, 2020.
+    filter_size = 2
+
+    # Construct model
+    model = Sequential()
+    model.add(Conv2D(filters=256, kernel_size=(5, 5), input_shape=(X_shape[1], X_shape[2], X_shape[3]), activation='relu'))
+    model.add(MaxPooling2D(pool_size=2))
+    #model.add(Dropout(0.2))
+
+    model.add(Conv2D(filters=256, kernel_size=(5, 5), activation='relu'))
+    model.add(MaxPooling2D(pool_size=2))
+    #model.add(Dropout(0.2))
+
+    #model.add(Conv2D(filters=64, kernel_size=2, activation='relu'))
+    #model.add(MaxPooling2D(pool_size=2))
+    #model.add(Dropout(0.2))
+
+    #model.add(Conv2D(filters=128, kernel_size=2, activation='relu'))
+    #model.add(MaxPooling2D(pool_size=2))
+    #model.add(Dropout(0.2))
+    model.add(GlobalAveragePooling2D())
+
+    model.add(Dense(64, activation='relu'))
+    model.add(Dense(nb_classes))
+
     return model
 
 
